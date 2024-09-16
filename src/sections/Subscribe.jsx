d7cf484bd42187736146416cbbf40bc65bbda930
Vikramgopal <vikrambg02@gmial.com>
2024-09-16 19:03:13 +0530

    changed subsribe button

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -25,24 +25,20 @@ const Subscribe = () => {
       <h3 className="lg:max-w-md font-bold font-palanquin leading-[68px] text-4xl">
         Sing Up for <span className="text-coral-red">Updates</span> & Newsletter
       </h3>
-      <form className="add-form" onSubmit={handleSubmit}>
-        <div className="flex items-center gap-5 p-2.5 sm:border rounded-full sm:border-slate-gray w-full lg:w-[40%] max-sm:flex-col">
-          <input
-            type="text"
-            placeholder="Subscribe@Nike.com"
-            value={description}
-            onChange={(e) => setDesciption(e.target.value)}
-            className="input"
-          />
-          <div>
-            <button
-              onClick={handleSubmit}
-              className="rounded-full px-7 py-4 border font-montserrat text-lg leading-none bg-black text-white border-black hover:shadow-lg shadow-slate-gray hover:bg-slate-gray hover:border-slate-gray transition-all duration-300 "
-            >
-              Subscribe Now{" "}
-            </button>
-          </div>
-        </div>
+      <form
+        className="flex items-center gap-5 p-2.5 sm:border rounded-full sm:border-slate-gray w-full lg:w-[40%] max-sm:flex-col"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="Subscribe@Nike.com"
+          value={description}
+          onChange={(e) => setDesciption(e.target.value)}
+          className="input"
+        />
+        <button className="rounded-full px-7 py-4 border font-montserrat text-lg leading-none bg-black text-white border-black hover:shadow-lg shadow-slate-gray hover:bg-slate-gray hover:border-slate-gray transition-all duration-300 ">
+          Subscribe Now{" "}
+        </button>
       </form>
     </section>
   );
